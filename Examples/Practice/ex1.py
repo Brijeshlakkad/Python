28228d8cc2471feb77ce32107c7fc46a5fcc6516
Brijesh Lakkad <brijeshlakkad22@gmail.com>
2017-10-29 11:36:55 +0530

    Added selenium automatic filling out gmail login

--- Examples/Practice/ex1.py
+++ Examples/Practice/ex1.py
@@ -2,8 +2,27 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+def sub():
+	nxt=browser.find_element_by_xpath("//content/span")
+	nxt.click()
+user=input("Enter Email Adress : ")
+password=input("Enter Password : ")
+print("calling firefox...")
 browser = webdriver.Firefox()
-browser.get('http://nostarch.com')
-htmlElem = browser.find_element_by_tag_name('html')
-htmlElem.send_keys(Keys.END)     # scrolls to bottom
-htmlElem.send_keys(Keys.HOME)    # scrolls to top
+print("connecting.... ")
+browser.get('https://mail.google.com')
+print("server connected :)")
+emailElem = browser.find_element_by_id('identifierId')
+emailElem.send_keys(user)
+sub()
+browser.implicitly_wait(100)
+passwordElem = browser.find_element_by_tag_name("div").find_element_by_name('password')
+browser.implicitly_wait(100)
+
+for i in password:
+	passwordElem.send_keys(i)
+	browser.implicitly_wait(10)
+
+passwordElem.send_keys(Keys.RETURN)
+
+print("done")
--- Examples/new/Web Scrapping/selenium/filling_out.py
+++ Examples/new/Web Scrapping/selenium/filling_out.py
@@ -1,21 +1,28 @@
 #!C:\Users\RAJ\AppData\Local\Programs\Python\Python36\python
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 def sub():
-	nxt=browser.find_element_by_tag_name("content").find_element_by_tag_name("span")
+	nxt=browser.find_element_by_xpath("//content/span")
 	nxt.click()
-
+user=input("Enter Email Adress : ")
+password=input("Enter Password : ")
+print("calling firefox...")
 browser = webdriver.Firefox()
+print("connecting.... ")
 browser.get('https://mail.google.com')
+print("server connected :)")
 emailElem = browser.find_element_by_id('identifierId')
-emailElem.send_keys('brijeshlakkad22@gmail.com')
+emailElem.send_keys(user)
 sub()
+browser.implicitly_wait(100)
+passwordElem = browser.find_element_by_tag_name("div").find_element_by_name('password')
+browser.implicitly_wait(100)
 
-#below will not run
-#i=0
-#while i<10:
-#	i=+1
-#passwordElem = browser.find_element_by_tag_name("div").find_element_by_name('password')
-#passwordElem.send_keys('124567267b')
-#sub()
+for i in password:
+	passwordElem.send_keys(i)
+	browser.implicitly_wait(10)
+
+passwordElem.send_keys(Keys.RETURN)
+
 print("done")
